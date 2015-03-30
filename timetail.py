83f32bd8a0ce4c15a35491526b50a4ad3e3bc4b6
Colin Fraser (LinuxIT) <colin.fraser@linuxit.com>
2015-03-30 21:35:07 +0100

    set the args

--- timetail.py
+++ timetail.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import argparse
 import sys
+import argparse
 import pprint
 
 
@@ -13,7 +13,7 @@ parser.add_argument('-p', '--period', help='Period of time for which to display 
 parser.add_argument('-i', '--input', help='Time and date format of the input file')
 parser.add_argument('-e', '--epoch', help='Output is displayed in UNIX epoch time format', action='store_true')
 parser.add_argument('-o', '--oldest', help='Display records oldest first', action='store_true')
-parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+parser.add_argument('infile', metavar='FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 args = parser.parse_args()
 
 pprint.pprint(args.infile.readlines())
