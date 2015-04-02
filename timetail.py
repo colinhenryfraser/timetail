1759ed34f400b1793b0093f726492fb18346bc53
Colin Fraser (LinuxIT) <colin.fraser@linuxit.com>
2015-04-02 23:48:59 +0100

    Competed the functional part

--- timetail.py
+++ timetail.py
@@ -8,6 +8,47 @@ import datetime
 import re
 import collections
 
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-t', '--time', default=3600, help='Time period to display data from log. Append ')
+	parser.add_argument('-i', '--input', metavar='DATE FORMAT', default='%b %d %X', help='Time and date format of the input file')
+	parser.add_argument('-e', '--epoch', help='Display date in UNIX epoch time format', action='store_true')
+	parser.add_argument('-o', '--oldest', help='Display oldest records last (reverse output).', action='store_true')
+	parser.add_argument('infile', metavar='FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Read data from FILE. With no FILE, read standard input.')
+	
+	return parser.parse_args()
+
+#
+# Test string for int
+def is_int(string):
+	try: 
+        	int(sting)
+        	return True
+    	except:
+        	return False
+
+
+def get_seconds(time_string):
+	#
+	# get the last char of the string to test
+	switch = time_string[-1:]
+
+	if switch == 'w':
+		# weeks
+		return int(time_string[:-1])*604800
+	elif switch == 'd':
+		# days
+		return int(time_string[:-1])*86400
+	elif switch == 'h':
+		# hours
+		return int(time_string[:-1])*3600
+	elif switch == 'm':
+		# minutes
+		return int(time_string[:-1])*60
+	else:
+		return int(time_string)
+			
+
 def datefregex(date_format):
 	regex=''
 	i=0
@@ -70,7 +111,7 @@ def datefregex(date_format):
 			regex += str(date_format[i])
 			i+=1
 	return regex
-	
+
 # process text
 #
 def process_line(line, time_period, input_format, epoch):
@@ -88,33 +129,41 @@ def process_line(line, time_period, input_format, epoch):
 	try:
 		#
 		# Find the date in the line
-		match = re.search(date_regex, line)
+		match = re.search(date_regex, line.replace("  ", " 0"))
 		#
 		# Add the year if it doesn't exist
 		found_date = match.group() + datetime.datetime.strftime(datetime.datetime.now(), '%Y')
 		line_epoch_date = datetime.datetime.strptime(found_date, input_format+"%Y").strftime('%s')
 		if int(line_epoch_date) >= int(now_epoch_date)-int(time_period):
-			if epoch == True:
-				return {line_epoch_date: re.sub(date_regex, line_epoch_date, line)}
-			else:
-				return {line_epoch_date: line}
+			return re.sub(date_regex, line_epoch_date, line.replace("  ", " 0"))
 	except:
 		return 1
+
+##########
+# Main
+#
 
 #
 # Set the arguments 
 #
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--time', default=60, help='Time period in seconds for which to display data from log')
-parser.add_argument('-i', '--input', metavar='DATE FORMAT', default='%b %d %X', help='Time and date format of the input file')
+parser.add_argument('-t', '--time', default="3600", help='Time period to display data from log. Append ')
+parser.add_argument('-i', '--input', metavar='DATE FORMAT', default="%b %d %X", help='Time and date format of the input file')
 parser.add_argument('-e', '--epoch', help='Display date in UNIX epoch time format', action='store_true')
-parser.add_argument('-o', '--oldest', help='Display oldest records first', action='store_true')
+parser.add_argument('-o', '--oldest', help='Display oldest records last (reverse output).', action='store_true')
 parser.add_argument('infile', metavar='FILE', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Read data from FILE. With no FILE, read standard input.')
+
 args = parser.parse_args()
 
 #
+# Check the inputs
+if re.match("^[0-9]*[w|d|h|m|0-9]$", args.time) == None:
+	parser.print_usage()
+	exit()
+
+#
 # output is empty dict
-output = {}
+output = []
 
 if args.infile:
 	if args.infile.name == '<stdin>':
@@ -124,9 +173,9 @@ if args.infile:
 		while sys.stdin in select.select([args.infile], [], [], 0)[0]:
 			line = args.infile.readline()
 			if line:
-				test_result = process_line(line.rstrip('\n').replace("  ", " 0"), args.time, args.input, args.epoch)
+				test_result = process_line(line.rstrip('\n'), get_seconds(args.time), args.input, args.epoch)
 				if test_result != None:
-					output.update(test_result)
+					output.append(test_result)
 			else:
 				break
 	else:
@@ -134,13 +183,25 @@ if args.infile:
 	# Input from a file
 	#
 		for line in iter(args.infile):
-			test_result = process_line(line.rstrip('\n').replace("  ", " 0"), args.time, args.input, args.epoch)
+			test_result = process_line(line.rstrip('\n'), get_seconds(args.time), args.input, args.epoch)
 			if test_result != None:
-				output.update(test_result)
+				output.append(test_result)
 
 #
-# Print in order
+# Sort the list
 if args.oldest:
-	pprint.pprint(collections.OrderedDict(sorted(output.items(), reverse=True)).items())
+	output = sorted(output, reverse=True)
 else:
-	pprint.pprint(collections.OrderedDict(sorted(output.items())).items())
+	output = sorted(output)
+
+#
+# Print line with date on epoch
+for line in output:
+	if args.epoch:
+		print line
+	else:
+		#
+		# get the epoch date
+		match = re.search('\d{10}', line)
+		str_date = datetime.datetime.strftime(datetime.datetime.fromtimestamp(float(match.group())), args.input)
+		print re.sub('\d{10}', str_date.replace(" 0", "  "), line)
